848a4555eedee68a9c5b977a495c57a16619bf15
Alexander Justo <102604349+AlexanderJusto@users.noreply.github.com>
2024-09-23 17:09:39 -0500

    Add files via upload

---
+++ src/app/public/pages/progress-course/progress-course.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {NgPlural} from "@angular/common";
+
+
+@Component({
+  selector: 'app-progress-course',
+  standalone: true,
+  imports: [
+    NgPlural
+  ],
+  templateUrl: './progress-course.component.html',
+  styleUrl: './progress-course.component.css'
+})
+export class ProgressCourseComponent {
+
+}
